0db29e8e4c54e0e81013774eb36ebd2df1005d09
formi86 <nicformichelli@gmail.com>
2022-11-17 23:32:04 -0300

    link & print format

--- 2022/Python/collatz.py
+++ 2022/Python/collatz.py
@@ -1,15 +1,16 @@
 '''
     Este simple programa es para probar casos de la conjetura de Collatz
-    Hay un video muy interesante donde se explica de que trata: https://www.youtube.com/watch?v=eHmee564FMo&t=536s&ab_channel=TheWildProject
+    Hay un video muy interesante donde se explica de que trata: https://www.youtube.com/watch?v=eHmee564FMo
 
 '''
 
-x = int(input("Ingrese su numero inicial: \n"))
+x = int(input("Ingrese su numero inicial: "))
 y = x
 z = y
+i = 0
 
 while x != 4 or y != 1 or z != 2:
-    print("\n" + str(round(x)) + "\n\n")
+    print(str(round(x)))
     if (x % 2):
         z = y
         y = x
@@ -18,3 +19,7 @@ while x != 4 or y != 1 or z != 2:
         z = y
         y = x
         x /= 2
+    i += 1
+
+print(
+    f'\nSu numero termino en la secuencia 4, 2, 1 despues de la repeticion #{i} !')
